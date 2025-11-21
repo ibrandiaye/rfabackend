a151886380299273c684278cc1ec5511d08173a6
ibrandiaye <ibra789ndiaye@gmail.com>
2025-11-21 13:31:40 +0000

    ddd1111556

--- app/repositories/UserRepository.php
+++ app/repositories/UserRepository.php
@@ -5,8 +5,8 @@ namespace App\Repositories;
 use App\User;
 
 class UserRepository extends RessourceRepository{
-    public function __construct(User $user)
+    public function __construct(User $users)
     {
-        $this->model = $user;
+        $this->model = $users;
     }
 }
